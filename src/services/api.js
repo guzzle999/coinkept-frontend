4233bf160dcbfe0b32c7852d416606b6904c9aa5
Amonrat <109088709+guzzle999@users.noreply.github.com>
2025-09-22 14:29:11 +0700

    fixed services

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,6 @@
-const API_BASE_URL = "/api";
+const API_BASE_URL = import.meta.env.DEV
+? import.meta.env.VITE_API_BASE_URL
+: import.meta.env.VITE_PUBLIC_API_BASE_URL;
 
 class ApiService {
   constructor() {
